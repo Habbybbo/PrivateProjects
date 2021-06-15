83534f196adecbaa892787b59bb2ef13db1a3431
Yuliyan Andreev <habybbo@gmail.com>
2021-06-15 22:59:45 +0300

    Problem
    Вика ми, че няма Object reference

---
+++ 03.OldestFamilyMember/Family.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        
+        public Dictionary<string, int> FamilyMembers { get; set; }
+
+        public Family()
+        {
+
+        }
+        public Family(string name, int age)
+        {
+
+        }
+
+        public void AddMember(Person member)
+        {
+            FamilyMembers.Add(member.Name, member.Age);
+
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = new Person();
+
+            foreach (var person in FamilyMembers)
+            {
+                if (person.Value >= oldestMember.Age)
+                {
+                    oldestMember.Name = person.Key;
+                    oldestMember.Age = person.Value;
+                }
+            }
+
+            return oldestMember;
+        }
+    }
+}
